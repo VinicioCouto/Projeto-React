55dd54c4ee21b571af45bb2477d261eda9c18d6a
Vinicio Couto <viniciogbi@gmail.com>
2025-02-14 17:03:42 -0300

    layout inicial

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -5,6 +5,8 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
+import Container from './components/layout/Container'
+
 function App() {
 
   return (
@@ -15,12 +17,14 @@ function App() {
         <li><Link to="/contact">Contato</Link></li>
         <li><Link to="/new-project">Novo Projeto</Link></li>
       </ul>
+      <Container>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/company" element={<Company />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/new-project" element={<NewProject />}/>
       </Routes>
+      </Container>
     </Router>
 
   )
