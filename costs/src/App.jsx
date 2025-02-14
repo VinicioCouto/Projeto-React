909d7302eeb7f4e430d6435f0e5dc0ff2954eab7
Vinicio Couto <viniciogbi@gmail.com>
2025-02-14 16:24:33 -0300

    Pages start

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <p>Oi Projeto</p>
-    </>
+    <Router>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/empresa">Empresa</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contatos />} />
+      </Routes>
+    </Router>
+
   )
 }
 
