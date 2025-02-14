013941839a3fd77261edea0e5e908eef45ed2f92
Vinicio Couto <viniciogbi@gmail.com>
2025-02-14 16:45:36 -0300

    pages criadas

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
 
 function App() {
 
@@ -7,13 +11,15 @@ function App() {
     <Router>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/empresa">Empresa</Link></li>
-        <li><Link to="/contato">Contato</Link></li>
+        <li><Link to="/company">Empresa</Link></li>
+        <li><Link to="/contact">Contato</Link></li>
+        <li><Link to="/new-project">Novo Projeto</Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/empresa" element={<Empresa />} />
-        <Route path="/contato" element={<Contatos />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/new-project" element={<NewProject />}/>
       </Routes>
     </Router>
 
