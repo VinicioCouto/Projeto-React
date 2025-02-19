fa092026263a2130d0259c45ed9fac962d17a902
Vinicio Couto <viniciogbi@gmail.com>
2025-02-19 16:35:33 -0300

    Styles Navbar

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,32 +1,30 @@
 import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
 
   return (
     <Router>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/company">Empresa</Link></li>
-        <li><Link to="/contact">Contato</Link></li>
-        <li><Link to="/new-project">Novo Projeto</Link></li>
-      </ul>
+      <Navbar />
       <Container customClass="min-height">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/new-project" element={<NewProject />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/new-project" element={<NewProject />} />
+        </Routes>
       </Container>
+      <Footer/>
     </Router>
-
+  
   )
 }
 
